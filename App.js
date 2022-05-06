e1556b834929454843c7ba26dc06edcde3280049
diegoschnur <diegoschnur@gmail.com>
2022-05-05 22:16:31 -0300

    Initial commit
    Start project

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View } from "react-native";
+
+import StackNavigation from "./src/Navigation/StackNavigation";
+
+const App = () => {
+  return (
+    <StackNavigation />
+  );
+}
+
+export default App;
